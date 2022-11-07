e0ef1461cbb08d0f3e6e3e227ef7752055419da0
ce20480 <ce20480@bristol.ac.uk>
2022-11-07 21:20:13 +0000

    Class inheritance learning
    learnt about super and how to create a subclass with its own attributes

--- Classes.py
+++ Classes.py
@@ -2,6 +2,7 @@
 learning about classes
 '''
 import math
+import random
 class MyFraction():
     '''Constructor is the init'''
     def __init__(self, num, den):# the init function will automatically be called when the class is called
@@ -51,6 +52,37 @@ print(fraction / fraction2) # now can output a nice print because of def __str__
 x = 1
 print(eval('x+1'))# normal use of eval is to evaluate a string not as a string
 
+'''
+Class inheritance - child classes which inherits the init function from the parents(the attributes)
+'''
 
+class NamedFraction(MyFraction):
+
+    def __init__(self, num, den, name=False):
+        '''
+        Attributes specific to child class and can only be done by using super() fucntion
+        '''
+        super().__init__(num,den)#initialise super class( which is the one that you have derived your new class from) this allows you to create more attributes in this child class
+        self.name = name
+
+    def sig_fig(self, n):
+        return round(super().eval(), n) #this is another way to do it ( this always gives you the inherited version of eval)
+        #return round(self.eval(), n) # if you do not change the function that you use from the parent in the child you can do it this way
+    
+    # def eval(self):
+    #     inv = 1 - round(super().eval(),0)
+    #     ran = random.normalvariate(1,0)
+    #     num = ran * round(super().eval(),0)
+    #     den = ran * inv
+    #     return MyFraction(num,den)
+    
+    def __str__(self):
+        return 'This is: ' + self.name + '\n' + \
+               '{:^5}'.format(self.num) + '\n' + \
+               '----' + '\n' + \
+               '{:^5}'.format(self.den)
+
+nfrac = NamedFraction(2,6, 'Third')
+print(nfrac)
 
 
