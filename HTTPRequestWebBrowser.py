53106cb93e3def547a1807c28eb3c88dd23b2e7b
ce20480 <ce20480@bristol.ac.uk>
2022-12-15 19:06:52 +0000

    Used urllib library to make web browser

--- HTTPRequestWebBrowser.py
+++ HTTPRequestWebBrowser.py
@@ -6,12 +6,22 @@ import socket
 
 mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
-cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
+cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()#encode turns the bytes into unicode
 mysock.send(cmd)
 
 while True:
     data = mysock.recv(512)
     if len(data) < 1:
         break
-    print(data.decode(),end='')
+    print(data.decode(),end='')#decode converts the unicode back into a string
 mysock.close()
+# even easier way to make a web browser using urllib
+import urllib.request, urllib.error, urllib.parse
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+counts = dict()
+for line in fhand:
+    print(line.decode().strip())
+    words = line.decode().split()
+    for word in words:
+        counts[word] = counts.get(word, 0) + 1 #.get() gets value from key but if key doesnt exists gives value of 2 argument
+print(counts)
